dbe72e485c18fc3153ffb346e01509ecd993ac06
juandapps <juandapps@hotmail.com>
2023-05-22 18:35:38 -0500

    ➕Create chess game

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import './style.css';
+import createGame from './components/POST.js';
+
+const firstGame = 'Chess';
+createGame(firstGame)
+  .then((gameId) => {
+    // eslint-disable-next-line no-console
+    console.log(`Game created: ${gameId}`);
+  })
+  .catch((error) => {
+    // eslint-disable-next-line no-console
+    console.error(`Error creating game: ${error}`);
+  });
