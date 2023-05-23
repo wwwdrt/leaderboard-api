37d318d18dddf5e5e0bcd260aace7e72e88d1b89
juandapps <juandapps@hotmail.com>
2023-05-22 21:13:57 -0500

    ➕Add entry point logic

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import './style.css';
-import createGame from './components/POST.js';
+import createGame from './components/Create.js';
+import refreshScores from './components/Refresh.js';
+import submitScore from './components/Submit.js';
 
 const firstGame = 'Chess';
+let gameId;
+
 createGame(firstGame)
-  .then((gameId) => {
+  .then((id) => {
+    gameId = id;
     // eslint-disable-next-line no-console
     console.log(`Game created: ${gameId}`);
   })
@@ -11,3 +16,43 @@ createGame(firstGame)
     // eslint-disable-next-line no-console
     console.error(`Error creating game: ${error}`);
   });
+
+const refreshBtn = document.getElementById('refresh-btn');
+const scoreForm = document.getElementById('score-form');
+
+refreshBtn.addEventListener('click', async () => {
+  try {
+    await refreshScores(gameId);
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error(`Error refreshing scores: ${error}`);
+  }
+});
+
+scoreForm.addEventListener('submit', async (event) => {
+  event.preventDefault();
+
+  const nameInput = document.getElementById('name');
+  const scoreInput = document.getElementById('score');
+
+  const name = nameInput.value.trim();
+  const score = scoreInput.value.trim();
+
+  if (name === '' || score === '') {
+    // eslint-disable-next-line no-console
+    console.error('Name and score are required');
+    return;
+  }
+
+  try {
+    await submitScore(gameId, name, score);
+    nameInput.value = '';
+    scoreInput.value = '';
+    // eslint-disable-next-line no-console
+    console.log('Score submitted successfully');
+    await refreshScores(gameId);
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error(`Error submitting score: ${error}`);
+  }
+});
